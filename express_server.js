25a2985cfaef59b46d91fa92db9538997cdb73ab
kevinmore80 <kevinmore@outlook.com>
2017-10-19 05:49:50 +0000

    added login feature

--- express_server.js
+++ express_server.js
@@ -28,8 +28,22 @@ function generateRandomString() {
   return text;
 }
 
+//Defining a global Users Object
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+}
 
-//-----------------------------
+
+//--------------------------------------------------
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -52,10 +66,21 @@ app.get("/urls/new", function (req, res) {
 
   let templateVars = {
     urlHolder: urlDatabase,
-    username: req.cookies["username"]
+    //username: req.cookies["username"]
+    user:users['user_id']
   };
   res.render("urls_new", templateVars); //automatically looks in Views folder
   //res.redirect(301, "/urls");
+});
+
+
+//Register end point for User Registration
+app.get("/register", function(req, res) {
+  let getNewData = {
+    userData: users,
+    //username: null
+  }
+  res.render("urls_reg", getNewData);
 });
 
 
@@ -65,7 +90,8 @@ app.get("/urls/:id", function(req, res) {
 
     urlHolder: urlDatabase,
     shortURL: req.params.id,
-    username: req.cookies["username"]
+    //username: req.cookies["username"]
+    user: users['user_id']
   };
   res.render("urls_show", templateVars);
 });
@@ -78,17 +104,33 @@ app.get("/u/:shortURL", (req, res) => {
 
 });
 
-//From the urls_index.ejs - The Index location for URLs
-app.get('/urls', function(req, res) {
 
+//GET request for a new login page - urls_login.ejs
+app.get("/login", function(req, res) {
+  let getNewData = {
+    userData: users,
+    //username: null
+  }
+  res.render("urls_login", getNewData);
+});
+
+
+//GET Request to display all URLs
+app.get('/urls', function(req, res) {
+  console.log("Users object ", users);
   let templateVars = {
 
     urlHolder: urlDatabase,
-    username: req.cookies["username"]
+    //username: req.cookies["username"]
+    user: users['user_id']
   };
   res.render("urls_index", templateVars);
 });
+
+
 //************************************************************************
+
+
 
 //POST Request to delete a URL
 app.post("/urls/:id/delete", function (req, res) {
@@ -99,6 +141,7 @@ app.post("/urls/:id/delete", function (req, res) {
 
 });
 
+
 //POST Request to update a URL
 app.post("/urls/:id", function (req, res) {
   var idToUpdate = req.params.id;
@@ -108,14 +151,41 @@ app.post("/urls/:id", function (req, res) {
 
 });
 
+//POST /register endpoint - add user info to global users object
+
+app.post("/register", function(req, res) {
+  let randomId = generateRandomString();
+  let newUser = {
+      id:randomId,
+      email: req.body.email,
+      password:req.body.password
+  };
+
+//Handling Error Conditions
+  if(!req.body.email || !req.body.password) {
+    res.status(400);
+    res.send('Please enter an Email Address AND Password');
+  }
+
+  for(let key in users) {
+    if(users[key].email == req.body.email) {
+      res.status(400);
+      res.send('Please enter a unique username AND password');
+    }
+  }
+
+  users[randomId] = newUser;
+
+  res.cookie('user_id', randomId);
+  res.redirect(301,"/urls");
+});
+
 //To add to urlDatabase a new short and long URL (new entry)
 app.post("/urls", function (req, res) {
   let randVal = generateRandomString();
   urlDatabase[randVal] = req.body.longURL;
-  console.log(req.body.longURL);
   res.redirect(301,req.body.longURL);
 });
-
 
 
 //****************************************************
@@ -123,10 +193,23 @@ app.post("/urls", function (req, res) {
 //POST Request to handle the login with username
 
 app.post("/login", function (req, res) {
-  console.log("Login username function being accessed");
 
-  res.cookie('username', req.body.username);
-  console.log(req.body.username);
+    for (let key in users) {
+      if(users[key].email == req.body.email) {
+        if(users[key].password == req.body.password){
+          res.cookie('user_id', key);
+          console.log(key);
+          res.redirect(301,'/')
+        } else {
+          res.status(403);
+          res.send('403 error, password not found');
+          }
+      } else {
+          res.status(403);
+          res.send('403 error, email not found');
+    }
+    }
+
   res.redirect(301,"/urls");
 
 });
@@ -134,12 +217,14 @@ app.post("/login", function (req, res) {
 //POST Request to handle the logout if username already exists
 
 app.post("/logout", function (req, res) {
-  console.log("Logout function being accessed");
+  //console.log("Logout function being accessed");
 
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect(301,"/urls");
 
 });
+
+
 
 //Listening to the appropriate post
 app.listen(PORT, () => {
